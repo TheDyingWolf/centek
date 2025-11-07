146ab2ecb6cb4ab64601874fe2e2656e57c5b469
PYXzakl <pyx42069@gmail.com>
2025-11-07 13:40:04 +0100

    created and added favicon, kinda fixed program (it runs but idk if correctly

--- Centek/Startup.cs
+++ Centek/Startup.cs
@@ -24,5 +24,32 @@ namespace Centek
 
             services.AddControllersWithViews();
         }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
     }
 }
