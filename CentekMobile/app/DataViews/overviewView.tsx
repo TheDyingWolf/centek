7c6c9d0919f8faf24a57e234361aa9bd062848a2
TheDyingWolf <123252155+TheDyingWolf@users.noreply.github.com>
2026-01-10 21:13:58 +0100

    Overview has updated filters for offline

--- CentekMobile/app/DataViews/overviewView.tsx
+++ CentekMobile/app/DataViews/overviewView.tsx
@@ -1,127 +1,156 @@
 import { ButtonComponent, DatePickerComponent, DropdownComponent, LoaderScreen, MultiSelectComponent } from '@/components/allComponents';
 import { gradientStyle, styles } from '@/components/styles';
-import { useGetStats } from '@/hooks/getHooks';
+import { useGetPayments, useGetStats, usePaymentDropdowns } from '@/hooks/getHooks';
 import { ScreenOrientation } from '@/services/utils';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Stack } from 'expo-router';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Modal, ScrollView, Text, useWindowDimensions, View } from 'react-native';
 
 
 export default function OverviewView() {
-    ScreenOrientation();
-    
-      const { width, height } = useWindowDimensions();
-      const isLandscape = width > height;
-    
-      const [accountIds, setAccountIds] = useState<number[]>([]);
-      const [mainCategoryIds, setMainCategoryIds] = useState<number[]>([]);
-      const [subCategoryIds, setSubCategoryIds] = useState<number[]>([]);
-      const [type, setType] = useState<boolean | undefined>(undefined);
-      const [modalVisible, setModalVisible] = useState(false);
-      const [fromDate, setFromDate] = useState(() => {
-        const now = new Date();
-        return new Date(now.getFullYear(), now.getMonth(), 1);
-      });
-      const [toDate, setToDate] = useState(new Date());
-    
-      // get stats data
-      const { stats, loading } = useGetStats(
-        accountIds,
-        mainCategoryIds, subCategoryIds,
-        type,
-        fromDate.toLocaleDateString('en-CA'),
-        toDate.toLocaleDateString('en-CA')
-      );
-    
-    
-      if (loading || stats === null) return <LoaderScreen loading={loading} title="Overview" children={undefined}></LoaderScreen>;
-    
-      const accountDropdown = stats.accounts.map(a => ({
-        label: a.name,
-        value: a.id,
-      }));
-    
-      const mainCategoriesDropdown = stats.mainCategories.map(c => ({
-        label: c.name,
-        value: c.id,
-      }));
-    
-      const subCategoriesDropdown = stats.subCategories.map(s => ({
-        label: s.name,
-        value: s.id,
-      }));
+  ScreenOrientation();
 
-    return (
-        <LinearGradient
-              {...gradientStyle}
-              style={[styles.background]}
-            >
-              <Stack.Screen options={{ title: 'Overview' , headerShown: (isLandscape) ? false : true}} />
-        
-              <Modal
-                animationType="slide"
-                transparent={true}
-                visible={isLandscape ? false : modalVisible}
-                onRequestClose={() => {
-                  setModalVisible(!modalVisible);
-                }}>
-                <View style={[styles.container, { padding: 16 }]}>
-                  <View style={styles.modalView}>
-                    <ScrollView style={styles.modalScroll}>
-                      <MultiSelectComponent
-                        data={accountDropdown}
-                        selecting="accounts"
-                        value={accountIds}
-                        onChange={setAccountIds}
-                      />
-        
-                      <MultiSelectComponent
-                        data={mainCategoriesDropdown}
-                        selecting="main categories"
-                        value={mainCategoryIds}
-                        onChange={setMainCategoryIds}
-                      />
-        
-                      <MultiSelectComponent
-                        data={subCategoriesDropdown}
-                        selecting="sub categories"
-                        value={subCategoryIds}
-                        onChange={setSubCategoryIds}
-                      />
-        
-                      <DropdownComponent
-                        data={[
-                          { label: 'All', value: undefined },
-                          { label: 'Income', value: true },
-                          { label: 'Expense', value: false },
-                        ]}
-                        value={type}
-                        dropdownLabel="type"
-                        onChange={setType}
-                      />
-                      <View style={styles.dateTimePickerContainer}>
-                        <View style={[styles.container, { padding: 0 }]}>
-                          <Text>FROM</Text>
-                          <View style={{ alignItems: "center" }}>
-                            <DatePickerComponent value={fromDate} onChange={setFromDate} />
-                          </View>
-                        </View>
-                        <View style={[styles.container, { paddingRight: 20 }]}>
-                          <Text>TO</Text>
-                          <View style={{ alignItems: "center" }}>
-                            <DatePickerComponent value={toDate} onChange={setToDate} />
-                          </View>
-                        </View>
-                      </View>
-                    </ScrollView>
-                    <ButtonComponent label={"Close Filters"} onPress={() => setModalVisible(false)} ></ButtonComponent>
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [accountIds, setAccountIds] = useState<number[]>([]);
+  const [mainCategoryIds, setMainCategoryIds] = useState<number[]>([]);
+  const [subCategoryIds, setSubCategoryIds] = useState<number[]>([]);
+  const [type, setType] = useState<boolean | undefined>(undefined);
+  const [fromDate, setFromDate] = useState(() => {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), 1);
+  });
+  const [toDate, setToDate] = useState(new Date());
+
+  const filters = {
+    accountIds: accountIds,
+    mainCategoryIds: mainCategoryIds,
+    subCategoryIds: subCategoryIds,
+    type: type,
+    fromDate: fromDate,
+    toDate: toDate
+  };
+
+  const { payments: payments, loading } = useGetPayments(
+    filters
+  );
+
+  const {
+    accountDropdown,
+    mainCategoriesDropdown,
+    subCategoriesDropdown,
+  } = usePaymentDropdowns();
+
+  const totalAmount = useMemo(() => {
+    return payments.reduce((sum, p) => {
+      return sum + (p.type ? p.amount : -p.amount);
+    }, 0);
+  }, [payments]);
+
+  if (loading || payments === null) return <LoaderScreen loading={loading} title="Stats" children={undefined}></LoaderScreen>;
+
+  const TableHeader = () => (
+    <View style={styles.rowHeader}>
+      <Text style={styles.headerCell}>Name</Text>
+      <Text style={styles.headerCell}>Amount</Text>
+      <Text style={styles.headerCell}>Account</Text>
+      <Text style={styles.headerCell}>Category</Text>
+      <Text style={styles.headerCell}>Note</Text>
+    </View>
+  );
+
+  const PaymentRow = ({ p }: any) => (
+    <View style={styles.row}>
+      <Text style={styles.cell}>{p.name}</Text>
+      <Text
+        style={[
+          styles.cell,
+          { color: p.type ? 'green' : 'red', fontWeight: 'bold' },
+        ]}
+      >
+        {p.type ? p.amount : -p.amount} €
+      </Text>
+      <Text style={styles.cell}>{p.account?.name}</Text>
+      <Text style={styles.cell}>
+        {p.mainCategory?.name} / {p.subCategory?.name}
+      </Text>
+      <Text style={styles.cell}>{p.note}</Text>
+    </View>
+  );
+
+  return (
+    <LinearGradient
+      {...gradientStyle}
+      style={[styles.background]}
+    >
+      <Stack.Screen options={{ title: 'Stats', headerShown: (isLandscape) ? false : true }} />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isLandscape ? false : modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={[styles.container, { padding: 16 }]}>
+          <View style={styles.modalView}>
+            <ScrollView style={styles.modalScroll}>
+              <MultiSelectComponent
+                data={accountDropdown}
+                selecting="accounts"
+                value={accountIds}
+                onChange={setAccountIds}
+              />
+
+              <MultiSelectComponent
+                data={mainCategoriesDropdown}
+                selecting="main categories"
+                value={mainCategoryIds}
+                onChange={setMainCategoryIds}
+              />
+
+              <MultiSelectComponent
+                data={subCategoriesDropdown}
+                selecting="sub categories"
+                value={subCategoryIds}
+                onChange={setSubCategoryIds}
+              />
+
+              <DropdownComponent
+                data={[
+                  { label: 'All', value: undefined },
+                  { label: 'Income', value: true },
+                  { label: 'Expense', value: false },
+                ]}
+                value={type}
+                dropdownLabel="type"
+                onChange={setType}
+              />
+              <View style={styles.dateTimePickerContainer}>
+                <View style={[styles.container, { padding: 0 }]}>
+                  <Text>FROM</Text>
+                  <View style={{ alignItems: "center" }}>
+                    <DatePickerComponent value={fromDate} onChange={setFromDate} />
                   </View>
                 </View>
-              </Modal>
-              {!isLandscape && (
-                <ButtonComponent label={"Open Filters"} onPress={() => setModalVisible(true)} />
-              )}
-            </LinearGradient>
-    );
+                <View style={[styles.container, { paddingRight: 20 }]}>
+                  <Text>TO</Text>
+                  <View style={{ alignItems: "center" }}>
+                    <DatePickerComponent value={toDate} onChange={setToDate} />
+                  </View>
+                </View>
+              </View>
+            </ScrollView>
+            <ButtonComponent label={"Close Filters"} onPress={() => setModalVisible(false)} ></ButtonComponent>
+          </View>
+        </View>
+      </Modal>
+      {!isLandscape && (
+        <ButtonComponent label={"Open Filters"} onPress={() => setModalVisible(true)} />
+      )}
+    </LinearGradient>
+  );
 }
