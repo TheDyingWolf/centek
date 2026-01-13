7894c08bd08dcb6ad4d151416f572746b692f3e8
PYXzakl <pyx42069@gmail.com>
2026-01-13 19:27:00 +0100

    MOBILE: accountsCategoriesView: added amount for each one

--- CentekMobile/app/DataViews/accountsCategoriesView.tsx
+++ CentekMobile/app/DataViews/accountsCategoriesView.tsx
@@ -1,25 +1,64 @@
 import { LoaderScreen } from '@/components/allComponents';
 import { gradientStyle, styles } from '@/components/styles';
-import { useGetAccounts, useGetMainCategories, useGetSubCategories } from '@/hooks/getHooks';
+import { useGetMainCategories, useGetPayments } from '@/hooks/getHooks';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { FlatList, Text, View } from 'react-native';
 
 
 export default function accountsCategoriesView() {
-    const { accounts, loading: loadingAccounts } = useGetAccounts();
     const { mainCategories, loading: loadingMainCategories } = useGetMainCategories();
-    const { subCategories, loading: loadingSubategories } = useGetSubCategories();
+    const { payments, loading: loadingPayments } = useGetPayments();
+
+    var amountOnAccounts = null;
+    var amountOnMainCategories = null;
+    var amountOnSubCategories = null;
+    // get total for everything
+    if (payments && payments.every(p => p.account)) {
+        amountOnAccounts = Object.values(
+            payments.reduce<Record<number, { id: Number, name: string; total: number }>>((acc, p) => {
+                acc[p.accountId] ??= { id: p.accountId, name: p.account!.name, total: 0 };
+                acc[p.accountId].total += p.type ? p.amount : -p.amount;
+                return acc;
+            }, {})
+        );
+
+        amountOnMainCategories = Object.values(
+            payments.reduce<Record<number, { id: Number, name: string; total: number }>>((mc, p) => {
+                if (p.mainCategory) {
+                    mc[p.mainCategoryId] ??= { id: p.mainCategoryId, name: p.mainCategory!.name, total: 0 };
+                    mc[p.mainCategoryId].total += p.type ? p.amount : -p.amount;
+                }
+                return mc;
+            }, {})
+        );
+
+        amountOnSubCategories = Object.values(
+            payments.reduce<Record<number, { id: Number, name: string; mainCategoryId: number; total: number }>>((sc, p) => {
+                if (p.subCategory) {
+                    sc[p.subCategoryId] ??= { id: p.subCategoryId, name: p.subCategory!.name, mainCategoryId: p.subCategory.mainCategoryId, total: 0 };
+                    sc[p.subCategoryId].total += p.type ? p.amount : -p.amount;
+                }
+                return sc;
+            }, {})
+        );
+    };
+
 
     const TableHeader = () => (
-        <View style={styles.rowHeader}>
+        <View style={[styles.rowHeader, { width: "100%" }]}>
             <Text style={styles.headerCell}>Name</Text>
+            <Text style={styles.headerCell}>Amount</Text>
         </View>
     );
 
     const TableRow = ({ item }: any) => (
         <View style={styles.row}>
             <Text style={styles.cell}>{item.name}</Text>
+            <Text style={[
+                styles.cell,
+                { color: item.total > 0 ? 'green' : 'red', fontWeight: 'bold' },
+            ]}>{Number.isNaN(Math.round(item.total * 100) / 100) ? "/" : (Math.round(item.total * 100) / 100)}</Text>
         </View>
     );
 
@@ -27,6 +66,7 @@ export default function accountsCategoriesView() {
         <View style={[styles.rowHeader, { width: "100%" }]}>
             <Text style={styles.headerCell}>Name</Text>
             <Text style={styles.headerCell}>Main Category</Text>
+            <Text style={styles.headerCell}>Amount</Text>
         </View>
     );
 
@@ -34,10 +74,14 @@ export default function accountsCategoriesView() {
         <View style={styles.row}>
             <Text style={styles.cell}>{item.name}</Text>
             <Text style={styles.cell}>{mainCategories.find(mc => mc.id === item.mainCategoryId)?.name}</Text>
-        </View>
+            <Text style={[
+                styles.cell,
+                { color: item.total > 0 ? 'green' : 'red', fontWeight: 'bold' },
+            ]}>{Number.isNaN(Math.round(item.total * 100) / 100) ? "/" : (Math.round(item.total * 100) / 100)}</Text>
+        </View >
     );
 
-    if (loadingAccounts || loadingMainCategories || loadingSubategories) return <LoaderScreen loading={loadingAccounts || loadingMainCategories || loadingSubategories} title="Stats" children={undefined}></LoaderScreen>;
+    if (loadingMainCategories || loadingPayments) return <LoaderScreen loading={loadingMainCategories || loadingPayments} title="Stats" children={undefined}></LoaderScreen>;
 
 
     return (
@@ -48,7 +92,7 @@ export default function accountsCategoriesView() {
             <View style={[styles.container, { alignItems: "stretch", paddingBottom: 20, paddingHorizontal: 10 }]}>
                 <Text style={styles.titleText}>ACCOUNTS</Text>
                 <FlatList
-                    data={accounts}
+                    data={amountOnAccounts}
                     keyExtractor={(item) => item.id.toString()}
                     ListHeaderComponent={TableHeader}
                     renderItem={({ item }) => <TableRow item={item} />}
@@ -58,7 +102,7 @@ export default function accountsCategoriesView() {
             <View style={[styles.container, { alignItems: "stretch", paddingBottom: 20, paddingHorizontal: 10 }]}>
                 <Text style={styles.titleText}>MAIN CATEGORIES</Text>
                 <FlatList
-                    data={mainCategories}
+                    data={amountOnMainCategories}
                     keyExtractor={(item) => item.id.toString()}
                     ListHeaderComponent={TableHeader}
                     renderItem={({ item }) => <TableRow item={item} />}
@@ -68,7 +112,7 @@ export default function accountsCategoriesView() {
             <View style={[styles.container, { alignItems: "stretch", paddingBottom: 20, paddingHorizontal: 10 }]}>
                 <Text style={styles.titleText}>SUB CATEGORIES</Text>
                 <FlatList
-                    data={subCategories}
+                    data={amountOnSubCategories}
                     keyExtractor={(item) => item.id.toString()}
                     ListHeaderComponent={SubCategoriesTableHeader}
                     renderItem={({ item }) => <SubCategoriesTableRow item={item} />}
