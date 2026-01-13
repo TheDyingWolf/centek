4b230a3e6c4b82467279936253a277f4dfe164b8
PYXzakl <pyx42069@gmail.com>
2026-01-13 21:40:47 +0100

    BACKEND: ApiKeyAuthAttribute: removed null reference

--- Centek/Filters/ApiKeyAuthAttribute.cs
+++ Centek/Filters/ApiKeyAuthAttribute.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Centek.Filters
 {
@@ -11,28 +12,34 @@ namespace Centek.Filters
     public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
     {
         private const string ApiKeyHeaderName = "ApiKey";
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-        {
 
-            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+        public async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next
+        )
+        {
+            if (
+                !context.HttpContext.Request.Headers.TryGetValue(
+                    ApiKeyHeaderName,
+                    out var potentialApiKey
+                )
+            )
             {
                 context.Result = new UnauthorizedResult();
                 return;
             }
 
-            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var configuration =
+                context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
             var apiKey = configuration.GetValue<string>("ApiKey");
 
-            if (!apiKey.Equals(potentialApiKey))
+            if (apiKey.IsNullOrEmpty() || !apiKey.Equals(potentialApiKey))
             {
                 context.Result = new UnauthorizedResult();
                 return;
             }
 
             await next();
-
         }
-
     }
-
 }
